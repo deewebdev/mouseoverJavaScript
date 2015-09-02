5f12d35304318ca4b062a95d9409219bda0edea4
deewebdev <WD003@SJC-316-021.sjcvec.ie>
2015-09-02 15:54:03 +0100

    mouseover
    nice javascript button feature

---
+++ mouseover.js
@@ -0,0 +1,20 @@
+
+        function MouseRollover(MyImage) {
+        MyImage.src = "images/buttonup1.png";
+    }
+        function MouseOut(MyImage) {
+        MyImage.src = "images/buttondown1.png";
+    }
+	        function MouseRollover2(MyImage2) {
+        MyImage2.src = "images/buttonup2.png";
+    }
+        function MouseOut2(MyImage2) {
+        MyImage2.src = "images/buttondown2.png";
+    }
+	
+	 function MouseRollover3(MyImage3) {
+        MyImage3.src = "images/buttonup3.png";
+    }
+        function MouseOut3(MyImage3) {
+        MyImage3.src = "images/buttondown3.png";
+    }
